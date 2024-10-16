3c2d05d5c80fb6b18f8f35425ebae0eb296bd1f4
satwikboina <satwikboina@gmail.com>
2024-10-16 21:49:00 +0530

    input and output functions

--- dsa/Arrays/reversal.py
+++ dsa/Arrays/reversal.py
@@ -3,9 +3,19 @@ import os
 inputPath = os.path.join(os.getcwd(),"dsa/input.txt")
 outputPath = os.path.join(os.getcwd(),"dsa/output.txt")
 
-with open(inputPath, "r") as infile:
-    input_data = infile.readlines()
-print(len(input_data))
+def read_input():
+    with open(inputPath, "r") as infile:
+        input_data = infile.readlines()
+    print(len(input_data))
+    return input_data
+
+def write_output(output_data="",mode = 'a'):
+    '''
+    modes are nothing but the file handling modes in python.
+    
+    '''
+    with open(outputPath,mode) as outfile:
+        outfile.write(output_data)
 
 
 def arrayReversal(input_data : list):
@@ -20,15 +30,18 @@ def arrayReversal(input_data : list):
         first +=1
     return input_data
 
+input_data = read_input()
+
+#writing None to the output file
+write_output(mode = 'w')
 
 for i in range(len(input_data)):
-    current_input = input_data[i].split(",")
+    current_input = input_data[i].strip().split(",")
     output_data = arrayReversal(current_input)
     print(output_data)
+    output_data = ", ".join(output_data)+"\n"
 
-    with open(outputPath,'a') as outfile:
-        outfile.writelines(", ".join(output_data)+"\n")
-    
+    write_output(output_data=output_data,mode = 'a')
     
 
 
