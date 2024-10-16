801d7070ff48400439b73cd3a5b9fbf1e50839ac
satwikboina <satwikboina@gmail.com>
2024-10-16 22:01:10 +0530

    utils folder

--- dsa/Arrays/reversal.py
+++ dsa/Arrays/reversal.py
@@ -1,47 +1,37 @@
+
 import os
+print(os.getcwd())
 
-inputPath = os.path.join(os.getcwd(),"dsa/input.txt")
-outputPath = os.path.join(os.getcwd(),"dsa/output.txt")
+from dsa.fileRead import write_output,read_input
 
-def read_input():
-    with open(inputPath, "r") as infile:
-        input_data = infile.readlines()
-    print(len(input_data))
-    return input_data
 
-def write_output(output_data="",mode = 'a'):
-    '''
-    modes are nothing but the file handling modes in python.
+
+
+
+# def arrayReversal(input_data : list):
     
-    '''
-    with open(outputPath,mode) as outfile:
-        outfile.write(output_data)
+#     first = 0
+#     last = len(input_data)-1
+#     while first < last : 
+#         temp = input_data[first]
+#         input_data[first] = input_data[last]
+#         input_data[last]=temp
+#         last -= 1
+#         first +=1
+#     return input_data
 
+# input_data = read_input()
 
-def arrayReversal(input_data : list):
-    
-    first = 0
-    last = len(input_data)-1
-    while first < last : 
-        temp = input_data[first]
-        input_data[first] = input_data[last]
-        input_data[last]=temp
-        last -= 1
-        first +=1
-    return input_data
+# #writing None to the output file
+# write_output(mode = 'w')
 
-input_data = read_input()
+# for i in range(len(input_data)):
+#     current_input = input_data[i].strip().split(",")
+#     output_data = arrayReversal(current_input)
+#     print(output_data)
+#     output_data = ", ".join(output_data)+"\n"
 
-#writing None to the output file
-write_output(mode = 'w')
-
-for i in range(len(input_data)):
-    current_input = input_data[i].strip().split(",")
-    output_data = arrayReversal(current_input)
-    print(output_data)
-    output_data = ", ".join(output_data)+"\n"
-
-    write_output(output_data=output_data,mode = 'a')
+#     write_output(output_data=output_data,mode = 'a')
     
 
 
