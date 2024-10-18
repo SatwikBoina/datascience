27568dbb00b64e52f88f0a17874637e4a1b7cfcd
satwikboina <satwikboina@gmail.com>
2024-10-18 19:38:52 +0530

    added readFiles.py

--- dsa/Arrays/reversal.py
+++ dsa/Arrays/reversal.py
@@ -1,37 +1,33 @@
 
-import os
-print(os.getcwd())
-
-from dsa.fileRead import write_output,read_input
 
 
 
 
 
-# def arrayReversal(input_data : list):
+def arrayReversal(input_data : list):
     
-#     first = 0
-#     last = len(input_data)-1
-#     while first < last : 
-#         temp = input_data[first]
-#         input_data[first] = input_data[last]
-#         input_data[last]=temp
-#         last -= 1
-#         first +=1
-#     return input_data
+    first = 0
+    last = len(input_data)-1
+    while first < last : 
+        temp = input_data[first]
+        input_data[first] = input_data[last]
+        input_data[last]=temp
+        last -= 1
+        first +=1
+    return input_data
 
-# input_data = read_input()
+input_data = read_input()
 
-# #writing None to the output file
-# write_output(mode = 'w')
+#writing None to the output file
+write_output(mode = 'w')
 
-# for i in range(len(input_data)):
-#     current_input = input_data[i].strip().split(",")
-#     output_data = arrayReversal(current_input)
-#     print(output_data)
-#     output_data = ", ".join(output_data)+"\n"
+for i in range(len(input_data)):
+    current_input = input_data[i].strip().split(",")
+    output_data = arrayReversal(current_input)
+    print(output_data)
+    output_data = ", ".join(output_data)+"\n"
 
-#     write_output(output_data=output_data,mode = 'a')
+    write_output(output_data=output_data,mode = 'a')
     
 
 
