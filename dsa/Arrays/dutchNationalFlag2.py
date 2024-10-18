e263f1f789e30b44a0d76f6b43c1cb9e11fea1f5
satwikboina <satwikboina@gmail.com>
2024-10-18 21:56:17 +0530

    dutch flag 2nd instance added

---
+++ dsa/Arrays/dutchNationalFlag2.py
@@ -0,0 +1,46 @@
+import os,sys
+sys.path.append(os.path.join(os.getcwd(),"dsa"))
+
+from utils.readFiles import readFiles
+readFiles = readFiles()
+
+#writing None to the output file
+readFiles.write_output(mode = 'w')
+
+#getting the input
+input_data = readFiles.read_input()
+
+# move all the negative elements to one side of an array
+# function
+
+def dutchFlag(input:list):
+
+    start = 0
+    last = len(input)-1
+    
+
+    while start <=last:
+        
+        if input[start]<0:
+            start+=1
+        elif input[start]>=0:
+            #swap with last element
+            input[start],input[last]=input[last],input[start]
+            #start+=1
+            last-=1
+        
+
+    return input
+    
+
+
+for i in range(len(input_data)):
+    
+    current_input = input_data[i].strip().split(",")
+    
+    current_input = [int(x) for  x in current_input]
+    output_data = dutchFlag(current_input)
+    print(output_data)
+    # output_data = ", ".join(output_data)+"\n"
+
+    readFiles.write_output(output_data=str(output_data)+"\n",mode = 'a')
